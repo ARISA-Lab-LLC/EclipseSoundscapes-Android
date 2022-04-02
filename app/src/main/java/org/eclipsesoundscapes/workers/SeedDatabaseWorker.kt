abd6a28dfd33843459633f6c134cfe7d151b345b
Joel Goncalves <goncalves.joel95@gmail.com>
2022-04-02 18:59:34 -0400

    Removes debug logs

--- app/src/main/java/org/eclipsesoundscapes/workers/SeedDatabaseWorker.kt
+++ app/src/main/java/org/eclipsesoundscapes/workers/SeedDatabaseWorker.kt
@@ -1,7 +1,6 @@
 package org.eclipsesoundscapes.workers
 
 import android.content.Context
-import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.google.gson.Gson
@@ -32,11 +31,9 @@ class SeedDatabaseWorker(
                     }
                 }
             } else {
-                Log.e(TAG, "Error seeding database - no valid filename")
                 Result.failure()
             }
         } catch (ex: Exception) {
-            Log.e(TAG, "Error seeding database", ex)
             Result.failure()
         }
     }
